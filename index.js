8964427a87f13b13be003c57bc4d4592cea7fdec
ca0v <coreyalix@hotmail.com>
2021-11-06 12:13:16 -0400

    1 file modified

--- index.js
+++ index.js
@@ -19,66 +19,121 @@ function stringify(o, terse = true) {
   return v.replaceAll(`"`, '');
 }
 
+function isString(o) {
+  return typeof o === 'string';
+}
+
+function sort(o) {
+  const result = {};
+  Object.keys(o)
+    .sort()
+    .forEach((k) => (result[k] = o[k]));
+  return result;
+}
+
+function consolidate(types) {
+  if (!types || types.length < 2) return types;
+  const result = {};
+  for (let i = 0; i < types.length; i++) {
+    Object.keys(types[i]).forEach((key) => {
+      const newType = types[i][key];
+      if (!result[key]) {
+        result[key] = newType;
+        return;
+      }
+      if (result[key] === newType) {
+        return;
+      }
+      if (isString(newType) && isString(result[key])) {
+        result[key] += '|' + newType;
+        return;
+      }
+      console.log('consolidate', key, result[key], newType);
+      consolidate([result[key], types[i][key]]);
+    });
+  }
+  return isString(result) ? result : sort(result);
+}
+
 function unionTypes(types) {
   if (!types?.length) return 'any';
-  const items = unique(types)
-    .filter((v) => !!v)
-    .map((v) => {
-      if (typeof v === 'string') return v;
-      if (typeof v === 'object') return stringify(v);
-      return v;
-    });
+  const prims = unique(types.filter((t) => typeof t === 'string'));
+
+  const complex = consolidate(types.filter((t) => typeof t === 'object'));
+
+  if (complex) prims.push(stringify(complex));
+
+  const items = prims;
   if (items.length === 1) return items[0];
   if (items.length > 3) return 'any';
   return items.join('|');
 }
 
 function typeOf(o) {
-  if (typeof o === '') return 'undefined';
-  if (typeof o === 'undefined') return 'undefined';
-  if (typeof o === 'null') return 'undefined';
-  if (typeof o === 'string') return 'string';
-  if (typeof o === 'number') return 'number';
-  if (typeof o === 'object') {
-    if (o instanceof Date) return 'Date';
-    if (Array.isArray(o)) {
-      const allTypes = o.map(typeOf);
-      return `Array<${unionTypes(allTypes)}>`;
+  const typeName = typeof o;
+  switch (typeName) {
+    case '':
+    case 'undefined':
+    case 'null':
+      return 'undefined';
+    case 'string':
+    case 'number':
+    case 'boolean':
+      return typeName;
+    case 'object': {
+      if (o instanceof Date) return 'Date';
+      if (Array.isArray(o)) {
+        const allTypes = o.map(typeOf);
+        return `Array<${unionTypes(allTypes)}>`;
+      }
+      const keys = Object.keys(o);
+      if (!keys.length) return 'unknown';
+      const result = {};
+      keys.forEach((k) => {
+        result[k] = typeOf(o[k]);
+      });
+      return result;
     }
-    const keys = Object.keys(o);
-    if (!keys.length) return 'unknown';
-    const result = {};
-    keys.forEach((k) => {
-      result[k] = typeOf(o[k]);
-    });
-    return result;
+    default:
+      return 'unknown';
   }
-  return 'unknown';
 }
 
 function asInterface(name, o) {
   return `interface ${name} ${stringify(typeOf(o), false)}`;
 }
 
-const sample = {
-  s: 'string',
-  n: 1,
-  e: {},
-  d: new Date(),
-  a: [1, '2'],
-  o: { a: [''], n: [0, 1], d: [new Date(), 10] },
-  ca: [{ a: 1 }, { b: 2 }, { a: 1, b: 1 }],
-  cx: {
-    a: 1,
-    b: [1],
-    c: { a: 1, b: [1, 2, 3, '', 4, 5, 7] },
-    d: { a: 1, b: [1, 'b', 2, new Date(), {}] },
-    e: { a: 1, b: [1] },
+const samples = [
+  {
+    items: [
+      0,
+      { a: 1, d: !0 },
+      { b: 1 },
+      { a: 2 },
+      { b: '', c: new Date() },
+      '',
+    ],
   },
-};
+  {
+    s: 'string',
+    n: 1,
+    e: {},
+    d: new Date(),
+    a: [1, '2'],
+    o: { a: [''], n: [0, 1], d: [new Date(), 10] },
+    ca: [{ a: 1 }, { b: 2 }, { a: 1, b: 1 }],
+    cx: {
+      a: 1,
+      b: [1],
+      c: { a: 1, b: [1, 2, 3, '', 4, 5, 7] },
+      d: { a: 1, b: [1, 'b', 2, new Date(), {}] },
+      e: { a: 1, b: [1] },
+    },
+  },
+];
 
-inputValue.value = JSON.stringify(sample, null, '  ');
-appDiv.value = `${asInterface('ITest', sample)}`;
+inputValue.value = JSON.stringify(samples[0], null, '  ');
+appDiv.value = `${asInterface('ITest', samples[0])}`;
 
 inputValue.onkeyup = () => {
   try {
