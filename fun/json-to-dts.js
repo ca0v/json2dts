c895f4c3c7e8bc32301da9f3fcf6db5a199284cd
Corey Alix <coreyalix@hotmail.com>
2021-11-06 14:53:53 -0400

    stackblitz isn't quite there yet....

--- fun/json-to-dts.js
+++ fun/json-to-dts.js
@@ -1,22 +1,12 @@
-import { asInterface } from "./fun/json-to-dts.js";
+export function jsontodts(o) {
+  return asInterface(o);
+}
 
-// Write Javascript code!
-const [appDiv, inputValue, log, nextButton] = [
-  "output_json",
-  "input_json",
-  "log",
-  "next",
-].map((k) => document.getElementById(k));
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
-// interesting...map is not called for empty element (2)
-log.innerText = [1, , 3].map((v, i) => `typeof ${i}=${typeof v}`).join(",");
-
-function unique(items) {
+export function unique(items) {
   return [...new Set(items)];
 }
 
-function stringify(o, terse = true) {
+export function stringify(o, terse = true) {
   const v = terse ? JSON.stringify(o) : JSON.stringify(o, null, "  ");
   return v.replaceAll(`"`, "");
 }
@@ -25,7 +15,11 @@ function isString(o) {
   return typeof o === "string";
 }
 
-function sort(o) {
+function isArray(o) {
+  return Array.isArray(o);
+}
+
+export function sort(o) {
   const result = {};
   Object.keys(o)
     .sort()
@@ -33,7 +27,7 @@ function sort(o) {
   return result;
 }
 
-function consolidate(types) {
+export function consolidate(types) {
   if (!types.length) return false;
   if (types.length < 2) return types;
   const result = {};
@@ -58,7 +52,7 @@ function consolidate(types) {
   return isString(result) ? result : sort(result);
 }
 
-function unionTypes(types) {
+export function unionTypes(types) {
   if (!types?.length) return "any";
   const prims = unique(types.filter((t) => typeof t === "string"));
 
@@ -72,7 +66,7 @@ function unionTypes(types) {
   return items.join("|");
 }
 
-function typeOf(o) {
+export function typeOf(o) {
   const typeName = typeof o;
   switch (typeName) {
     case "":
@@ -85,7 +79,7 @@ function typeOf(o) {
       return typeName;
     case "object": {
       if (o instanceof Date) return "Date";
-      if (Array.isArray(o)) {
+      if (isArray(o)) {
         const allTypes = o.map(typeOf);
         return `Array<${unionTypes(allTypes)}>`;
       }
@@ -102,60 +96,6 @@ function typeOf(o) {
   }
 }
 
-const samples = [
-  {
-    a: [{ a: 1 }, { a: !1 }, { b: "" }, { b: 0, a: new Date() }],
-  },
-  {
-    array1: [0],
-    array2: ["", !0],
-    array3: ["", !0, 0],
-    array4: ["", !0, new Date()],
-    array5: ["", !0, 0, new Date()],
-    items: [
-      0,
-      { a: 1, d: !0 },
-      { b: 1 },
-      { a: 2 },
-      { b: "", c: new Date() },
-      "",
-    ],
-  },
-  {
-    s: "string",
-    n: 1,
-    e: {},
-    d: new Date(),
-    a: [1, "2"],
-    o: { a: [""], n: [0, 1], d: [new Date(), 10] },
-    ca: [{ a: 1 }, { b: 2 }, { a: 1, b: 1 }],
-    cx: {
-      a: 1,
-      b: [1],
-      c: { a: 1, b: [1, 2, 3, "", 4, 5, 7] },
-      d: { a: 1, b: [1, "b", 2, new Date(), {}] },
-      e: { a: 1, b: [1] },
-    },
-  },
-];
-
-inputValue.value = JSON.stringify(samples[0], null, "  ");
-appDiv.value = `${asInterface("ITest", samples[0])}`;
-
-inputValue.onkeyup = () => {
-  try {
-    let o;
-    eval(`o = ${inputValue.value}`);
-    appDiv.value = `${asInterface("ITest", o)}`;
-  } catch (ex) {
-    log.innerText = ex;
-  }
-};
-
-let sampleIndex = 0;
-nextButton.onclick = () => {
-  sampleIndex = (1 + sampleIndex) % samples.length;
-
-  inputValue.value = JSON.stringify(samples[sampleIndex], null, "  ");
-  appDiv.value = `${asInterface("ITest", samples[sampleIndex])}`;
-};
+export function asInterface(name, o) {
+  return `interface ${name} ${stringify(typeOf(o), false)}`;
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+import { assert } from "chai";
+import { jsontodts } from "./fun/json-to-dts.js";
+
+describe("test", () => {
+  it("test", () => {
+    assert.isFunction(jsontodts);
+  });
+});
