3043f70dfd335126680d8a6433d2731a6c9d8517
Corey Alix <coreyalix@hotmail.com>
2021-11-12 22:20:47 -0500

    Runs against tested version

---
+++ bin/index.js
@@ -0,0 +1,85 @@
+import { full } from "./fun/full.js";
+const myFull = (o) => `interface I ${full(o)};`;
+// Write Javascript code!
+const inputValue = document.getElementById("input_json");
+const outputValue = document.getElementById("output_json");
+const [log, nextButton] = [
+    "log",
+    "next",
+].map((k) => document.getElementById(k));
+outputValue.innerHTML = `<h1>JS Starter</h1>`;
+const samples = [
+    { a: [{ b: [1] }] },
+    {
+        a: [
+            { a: 1 },
+            { a: !1 },
+            { b: "" },
+            { b: 0, a: new Date() },
+        ],
+    },
+    {
+        array1: [0],
+        array2: ["", !0],
+        array3: ["", !0, 0],
+        array4: ["", !0, new Date()],
+        array5: ["", !0, 0, new Date()],
+        items: [
+            0,
+            { a: 1, d: !0 },
+            { b: 1 },
+            { a: 2 },
+            { b: "", c: new Date() },
+            "",
+        ],
+    },
+    {
+        s: "string",
+        n: 1,
+        e: {},
+        d: new Date(),
+        a: [1, "2"],
+        o: {
+            a: [""],
+            n: [0, 1],
+            d: [new Date(), 10],
+        },
+        ca: [
+            { a: 1 },
+            { b: 2 },
+            { a: 1, b: 1 },
+        ],
+        cx: {
+            a: 1,
+            b: [1],
+            c: {
+                a: 1,
+                b: [1, 2, 3, "", 4, 5, 7],
+            },
+            d: {
+                a: 1,
+                b: [1, "b", 2, new Date(), {}],
+            },
+            e: { a: 1, b: [1] },
+        },
+    },
+];
+function render() {
+    try {
+        let o;
+        eval(`o = ${inputValue.value}`);
+        outputValue.value = `${myFull(o)}`;
+    }
+    catch (ex) {
+        log.innerText = ex;
+    }
+}
+inputValue.onkeyup = () => render();
+let sampleIndex = -1;
+nextButton.onclick = () => {
+    sampleIndex =
+        (1 + sampleIndex) % samples.length;
+    inputValue.value = JSON.stringify(samples[sampleIndex], null, "  ");
+    render();
+};
+nextButton.click();
---
+++ bin/test.js
@@ -0,0 +1,247 @@
+import { expect } from "chai";
+import { typedef } from "./fun/typedef.js";
+import { deep } from "./fun/deep.js";
+import { reduce } from "./fun/reduce.js";
+import { typing } from "./fun/typing.js";
+import { full } from "./fun/full.js";
+function log(o) {
+    console.log("_____ START ______________");
+    console.log(o);
+    console.log("_____ END ______________");
+    return o;
+}
+describe("typedef", () => {
+    it("typedef simple", () => {
+        expect(log(typedef({
+            a: [1, !1],
+        }))).deep.equal({
+            a: [["number", "boolean"]],
+        }, "typedef simple mixed array");
+        expect(log(typedef({
+            a: 1,
+            b: [!1],
+            c: "c",
+        }))).deep.equal({
+            a: ["number"],
+            b: [["boolean"]],
+            c: ["string"],
+        }, "simple typedef");
+    });
+    it("typedef moderate", () => {
+        expect(log(typedef({
+            a: 1,
+            b: [
+                1,
+                {
+                    c: 2,
+                },
+            ],
+        }))).deep.equal({
+            a: ["number"],
+            b: [
+                [
+                    "number",
+                    {
+                        c: ["number"],
+                    },
+                ],
+            ],
+        }, "simple moderate");
+    });
+    it("typedef advanced", () => {
+        expect(log(typedef({
+            a: 1,
+            b: [
+                1,
+                {
+                    c: 2,
+                },
+            ],
+        }))).deep.equal({
+            a: ["number"],
+            b: [
+                [
+                    "number",
+                    {
+                        c: ["number"],
+                    },
+                ],
+            ],
+        }, "typedef advanced 1");
+        expect(log(typedef({
+            a: [1, !1],
+            b: [!1],
+            c: {
+                d: "d",
+            },
+        }))).deep.equal({
+            a: [["number", "boolean"]],
+            b: [["boolean"]],
+            c: [
+                {
+                    d: ["string"],
+                },
+            ],
+        }, "typedef advanced 2");
+    });
+});
+describe("deep", () => {
+    it("simple deep", () => {
+        const o1 = {
+            a: 1,
+        };
+        const o2 = {
+            b: [1, { c: 2 }],
+        };
+        const o3 = {
+            a: 1,
+            b: [1, { c: 2 }],
+        };
+        expect(log(deep(o1, o2))).deep.equal(o3, "simple deep");
+    });
+    it("moderate deep", () => {
+        const o1 = {
+            a: 1,
+            b: [1, { c: 2 }],
+        };
+        const o2 = {
+            a: 1,
+            b: [1, { c: 2 }],
+        };
+        const o3 = {
+            a: 1,
+            b: [1, { c: 2 }, 1, { c: 2 }],
+        };
+        expect(log(deep(o1, o2))).deep.equal(o3, "moderate deep");
+    });
+    it("deep other", () => {
+        // {"a":1}, {"b":[[1,{"c":1}]]}
+        expect(log(deep({ a: 1 }, {
+            b: [
+                [
+                    1,
+                    {
+                        c: 1,
+                    },
+                ],
+            ],
+        }))).deep.equal({
+            a: 1,
+            b: [
+                [
+                    1,
+                    {
+                        c: 1,
+                    },
+                ],
+            ],
+        }, "deep other");
+    });
+});
+describe("reduce", () => {
+    it("simple reduce", () => {
+        expect(log(reduce([[1], [1]]))).deep.equal([[1]], "simple reduce [1]");
+        expect(log(reduce([
+            {
+                a: [1],
+            },
+            {
+                a: [1],
+            },
+        ]))).deep.equal([{ a: [1] }], "simple reduce a:[1]");
+        const o1 = {
+            n: 1,
+            a: [2],
+        };
+        const o2 = {
+            n: 1,
+            a: [2],
+        };
+        const o3 = {
+            n: 1,
+            a: [2],
+        };
+        expect(log(reduce([o1, o2]))).deep.equal([o3], "simple reduce 2");
+    });
+    it("moderate reduce", () => {
+        const o1 = {
+            a: [1],
+        };
+        const o2 = {
+            b: [
+                [
+                    1,
+                    {
+                        c: [1],
+                    },
+                ],
+            ],
+        };
+        const o3 = {
+            a: [1],
+            b: [
+                [
+                    1,
+                    {
+                        c: [1],
+                    },
+                ],
+            ],
+        };
+        expect(log(reduce([o1, o2]))).deep.equal([o3], "moderate reduce");
+    });
+    it("advanced reduce", () => {
+        const o1 = {
+            a: [1],
+            b: [[1, 1, 1]],
+        };
+        const o2 = {
+            b: [{ c: [1] }],
+        };
+        const o3 = {
+            a: [1],
+            b: [[1], { c: [1] }],
+        };
+        expect(log(reduce([o1, o2]))).deep.equal([o3], "advanced reduce");
+    });
+});
+describe("typing", () => {
+    it("simple typing", () => {
+        expect(log(typing({}))).equal("{}");
+        expect(log(typing({
+            a: "number",
+        }))).equal(`{a:number}`);
+        expect(log(typing({
+            a: ["number", "string"],
+        }))).equal(`{a:number|string}`);
+    });
+});
+describe("object->typing", () => {
+    it("simple", () => {
+        expect(log(full({
+            a: 1,
+        }))).equal("{a:number}");
+        expect(log(full({
+            a: [1],
+        }))).equal("{a:Array<number>}");
+    });
+    it("moderate", () => {
+        expect(log(full({
+            a: 1,
+            b: [1, !1],
+            c: [{ d: 1 }, { d: !1 }],
+        }))).equal("{a:number,b:Array<boolean|number>,c:Array<{d:boolean|number}>}");
+    });
+    it("advanced", () => {
+        expect(log(full({
+            a: 1,
+            b: [1, !1, "1"],
+            c: [
+                { d: 2 },
+                { d: !1 },
+                { d: 1 },
+                { d: !1 },
+            ],
+        }))).equal("{a:number,b:Array<boolean|number|string>,c:Array<{d:boolean|number}>}");
+    });
+});
